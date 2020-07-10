6cc697ffffa8cfdb1fb816b29700403b3b668e88
Cristian <borgesvive@hotmail.com>
2020-07-10 19:40:50 -0300

    Termina el punto 6 del ejercicio 003

--- grupos/grupo6/003-ejercicios/src/6.delay.js
+++ grupos/grupo6/003-ejercicios/src/6.delay.js
@@ -1,8 +1,10 @@
 /**
  * - Modificar nuestra función `delay` para que utilice Promises.
- * `delay` tomará un sólo parámetro `segundos` y debe devolver una Promise que resuelva cuando el tiempo ha finalizado.
+ *  `delay` tomará un sólo parámetro `segundos`
+ *   y debe devolver una Promise que resuelva cuando el tiempo ha finalizado.
  *
- * - Una vez modificada, cambiar nuestra función run() para que muestre en orden los siguientes mensajes.
+ * - Una vez modificada, cambiar nuestra función run()
+ *   para que muestre en orden los siguientes mensajes.
  *
  * 1
  * Terminó 1
@@ -21,13 +23,13 @@ function delay(mensaje, segundos) {
 
 let run = async () => {
   console.log(1);
-  const res1 = delay('Terminó 1', 3000);
+  const res1 = await delay('Terminó 1', 3000);
   console.log(res1);
   console.log(2);
-  const res2 = delay('Terminó 2', 2000);
+  const res2 = await delay('Terminó 2', 2000);
   console.log(res2);
   console.log(3);
-  const res3 = delay('Terminó 3', 1000);
+  const res3 = await delay('Terminó 3', 1000);
   console.log(res3);
 };
 
