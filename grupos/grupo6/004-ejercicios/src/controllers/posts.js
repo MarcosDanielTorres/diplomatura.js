ef3c3bb670e0852dc824001d430cf11768de0e2d
Cristian <borgesvive@hotmail.com>
2020-07-15 17:19:19 -0300

    Pasa promises a awaits.

--- grupos/grupo6/004-ejercicios/src/controllers/posts.js
+++ grupos/grupo6/004-ejercicios/src/controllers/posts.js
@@ -1,54 +1,50 @@
 import express from 'express';
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
 var postApi = express.Router();
 
-postApi.get('/', function (req, res) {
-    fetch('https://jsonplaceholder.typicode.com/posts/')
-       .then(response => response.json())
-        .then(posts => {
-            fetch('https://jsonplaceholder.typicode.com/users/')
-                .then(response2 => response2.json())
-                .then(users => {
+postApi.get('/', async function (req, res) {
+  const [rawPosts, rawUsers] = await Promise.all([
+    fetch('https://jsonplaceholder.typicode.com/posts/'),
+    fetch('https://jsonplaceholder.typicode.com/users/'),
+  ]);
 
-                    let lista = posts.map(value => {
-                        let resultadoFinal = {
-                        id: value.id,
-                        title: value.title,
-                        body: value.body,
-                        user: users.filter((user) => user.id === value.userId ),
-                        }
-                        return resultadoFinal;
-                    });
-                    res.send(lista)
-                })
-        })
+  const [posts, users] = await Promise.all([rawPosts.json(), rawUsers.json()]);
+
+  let lista = posts.map((element) => ({
+    id: element.id,
+    title: element.title,
+    body: element.body,
+    user: users.filter((user) => user.id === element.userId),
+  }));
+
+  res.send(lista);
 });
 
-
 postApi.get('/:id', function (req, res) {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${req.params.id}/`)
-       .then(response => response.json())
-        .then(posts => {
-            fetch(`https://jsonplaceholder.typicode.com/posts/${req.params.id}/comments/`)
-                .then(response2 => response2.json())
-                .then(comments => {
-                    let lista =  {
-                        userId: posts.userId,
-                        id: posts.id,
-                        title: posts.title,
-                        body: posts.body,
-                        posts:
-                            comments.map(value => ({
-                                id : value.id,
-                                name: value.name,
-                                email: value.email,
-                                body: value.body
-                            }))
-                        }
-                    res.send(lista)
-                });
-        })
-})
+  fetch(`https://jsonplaceholder.typicode.com/posts/${req.params.id}/`)
+    .then((response) => response.json())
+    .then((posts) => {
+      fetch(
+        `https://jsonplaceholder.typicode.com/posts/${req.params.id}/comments/`
+      )
+        .then((response2) => response2.json())
+        .then((comments) => {
+          let lista = {
+            userId: posts.userId,
+            id: posts.id,
+            title: posts.title,
+            body: posts.body,
+            posts: comments.map((value) => ({
+              id: value.id,
+              name: value.name,
+              email: value.email,
+              body: value.body,
+            })),
+          };
+          res.send(lista);
+        });
+    });
+});
 
 export default postApi;
