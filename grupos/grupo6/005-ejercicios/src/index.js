e8a04adde7d9db7df86c852ce383dfbe9a66e356
Carolina Villegas <caro.villegas63@gmail.com>
2020-07-18 11:04:34 -0300

    agrego solo un console log para una prueba

--- grupos/grupo6/005-ejercicios/src/index.js
+++ grupos/grupo6/005-ejercicios/src/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import alumnosRoutes from './controllers/alumnos';
+import materiasRoutes from './controllers/materias';
+import profesoresRoutes from './controllers/profesores';
 import calificacionesRoutes from './controllers/calificaciones';
 
 const PORT = 8080;
@@ -8,6 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use('/alumnos', alumnosRoutes);
+app.use('/materias', materiasRoutes);
+app.use('/profesores', profesoresRoutes);
 app.use('/calificaciones', calificacionesRoutes);
 app.get('/', function (req, res) {
   res.json({ mensaje: 'Bienvenido al servidor de la Universidad' });
