0cb72a34581b09482f4e04738ca9fed055573cc0
Cristian <borgesvive@hotmail.com>
2020-07-23 20:33:43 -0300

    Corrige ejercicio 4

--- grupos/grupo6/006-ejercicios/src/index4.js
+++ grupos/grupo6/006-ejercicios/src/index4.js
@@ -1,21 +1,33 @@
-let arreglo=Array.from({length: getRandomInt(10,31)}, () => ['']);
+const COLORES = ['red', 'yellow', 'green'];
+const cantidad = getRandomInt(10, 31);
+let clics = 0;
+
+function addFigure(type, color) {
+  const app = document.getElementById('app');
+  const figure = document.createElement('div');
+  figure.textContent = '¡Explotar!';
+  figure.setAttribute('class', type);
+  figure.style.setProperty('background', color);
+  figure.addEventListener('click', () => {
+    clics++;
+    figure.remove();
+    if (clics > cantidad) {
+      const h1 = document.createElement('h1');
+      h1.textContent = '¡Ganaste!';
+      app.appendChild(h1);
+    }
+  });
+  app.appendChild(figure);
+}
+
+for (let i = 0; i <= cantidad; i++) {
+  addFigure('circle', COLORES[Math.floor(Math.random() * COLORES.length)]);
+}
+
+// let arreglo = Array.from({ length: getRandomInt(10, 31) }, () =>
+//   addFigure('circle', 'red')
+// );
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  }
-let btnRed = document.getElementById('buttonExplotarRojo');
-
-btnRed.addEventListener('click', function () {
-    btnRed.remove();
-});
-
-let btnAzul = document.getElementById('buttonExplotarAzul');
-
-btnAzul.addEventListener('click', function () {
-    btnAzul.remove();
-});
-let btnYellow = document.getElementById('buttonExplotarAmarillo');
-
-btnYellow.addEventListener('click', function () {
-    btnYellow.remove();
-});
+  return Math.floor(Math.random() * (max - min)) + min;
+}
