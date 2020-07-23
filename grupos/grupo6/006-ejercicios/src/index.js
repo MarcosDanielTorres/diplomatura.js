db3b4c81cda7d5bbe7732377ec38bb273f0982ac
Cristian <borgesvive@hotmail.com>
2020-07-23 19:02:52 -0300

    Simplifica primer ejercicio

--- grupos/grupo6/006-ejercicios/src/index.js
+++ grupos/grupo6/006-ejercicios/src/index.js
@@ -1,90 +1,66 @@
 const MOUNTAINS = [
-    { name: 'Kilimanjaro', height: 5895, place: 'Tanzania' },
-    { name: 'Everest', height: 8848, place: 'Nepal' },
-    { name: 'Mount Fuji', height: 3776, place: 'Japan' },
-    { name: 'Vaalserberg', height: 323, place: 'Netherlands' },
-    { name: 'Denali', height: 6168, place: 'United States' },
-    { name: 'Popocatepetl', height: 5465, place: 'Mexico' },
-    { name: 'Mont Blanc', height: 4808, place: 'Italy/France' },
-  ];
-  
-  // let body = document.getElementsByTagName('body')[0];
-  
-  const tableCreate = (idioma) => {
-    let body = document.getElementsByTagName('body')[0];
-    let tabla = document.createElement('table');
-    let tbdy = document.createElement('tbody');
-    let tr = document.createElement('tr');
-    for (let i = 0; i < 3; i++) {
-      if (i == 0) {
-        let th = document.createElement('th');
-        idioma
-          ? th.appendChild(document.createTextNode('name'))
-          : th.appendChild(document.createTextNode('nombre'));
-        tr.appendChild(th);
-      } else if (i == 1) {
-        let th = document.createElement('th');
-        idioma
-          ? th.appendChild(document.createTextNode('height'))
-          : th.appendChild(document.createTextNode('altura'));
-        tr.appendChild(th);
-      } else {
-        let th = document.createElement('th');
-        idioma
-          ? th.appendChild(document.createTextNode('place'))
-          : th.appendChild(document.createTextNode('lugar'));
-        tr.appendChild(th);
-      }
-    }
-    tbdy.appendChild(tr);
-    tabla.appendChild(tbdy);
-    body.appendChild(tabla);
-  
-    agregarInfoTabla();
+  { name: 'Kilimanjaro', height: 5895, place: 'Tanzania' },
+  { name: 'Everest', height: 8848, place: 'Nepal' },
+  { name: 'Mount Fuji', height: 3776, place: 'Japan' },
+  { name: 'Vaalserberg', height: 323, place: 'Netherlands' },
+  { name: 'Denali', height: 6168, place: 'United States' },
+  { name: 'Popocatepetl', height: 5465, place: 'Mexico' },
+  { name: 'Mont Blanc', height: 4808, place: 'Italy/France' },
+];
+
+function createHeader(idioma) {
+  let tr = document.createElement('tr');
+
+  const idiomas = {
+    english: ['name', 'height', 'place'],
+    spanish: ['nombre', 'altura', 'lugar'],
   };
-  
-  const agregarInfoTabla = () => {
-    let body = document.getElementsByTagName('body')[0];
-    let tabla = document.createElement('table');
-    let tbdy = document.createElement('tbody');
-    for (let j = 0; j < MOUNTAINS.length; j++) {
-      let tr = document.createElement('tr');
-      for (let i = 0; i < 3; i++) {
-        if (i == 0) {
-          let td = document.createElement('td');
-          td.appendChild(document.createTextNode(MOUNTAINS[j].name));
-          console.log(MOUNTAINS[j].place);
-          tr.appendChild(td);
-        } else if (i == 1) {
-          let td = document.createElement('td');
-          td.appendChild(document.createTextNode(MOUNTAINS[j].height));
-          tr.appendChild(td);
-        } else {
-          let td = document.createElement('td');
-          td.appendChild(document.createTextNode(MOUNTAINS[j].place));
-          tr.appendChild(td);
-        }
-      }
-      tbdy.appendChild(tr);
-    }
-    tabla.appendChild(tbdy);
-    body.appendChild(tabla);
-  };
-  
-  
-  let body = document.getElementsByTagName('body')[0];
-  let btn = document.getElementById('btn1');
-  
-  btn.addEventListener('click', function(){
-      body.removeChild(body.lastChild);
-      body.removeChild(body.lastChild);
-      tableCreate(true);
-  });
-  
-  let btn2 = document.getElementById('btn2');
-  
-  btn2.addEventListener('click', function(){
-      body.removeChild(body.lastChild);
-      body.removeChild(body.lastChild);
-      tableCreate(false);
-  });
+
+  idiomas[idioma].forEach((name) => tr.appendChild(createNode(name, 'th')));
+
+  return tr;
+}
+
+function createEntry(obj) {
+  let tr = document.createElement('tr');
+
+  for (const propiedad in obj) {
+    tr.appendChild(createNode(obj[propiedad], 'td'));
+  }
+
+  return tr;
+}
+
+function createNode(text, type) {
+  let th = document.createElement(type);
+
+  th.textContent = text;
+
+  return th;
+}
+
+const tableCreate = (idioma) => {
+  let app = document.getElementsByTagName('div')[0];
+
+  let tabla = document.createElement('table');
+
+  tabla.appendChild(createHeader(idioma));
+
+  app.appendChild(tabla);
+
+  MOUNTAINS.forEach((mountain) => tabla.appendChild(createEntry(mountain)));
+};
+
+let div = document.getElementsByTagName('div')[0];
+let btn1 = document.getElementById('btn1');
+let btn2 = document.getElementById('btn2');
+
+btn1.addEventListener('click', function () {
+  div.removeChild(div.lastChild);
+  tableCreate('english');
+});
+
+btn2.addEventListener('click', function () {
+  div.removeChild(div.lastChild);
+  tableCreate('spanish');
+});
